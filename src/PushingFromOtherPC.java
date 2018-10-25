e2b9fb5950df952cfbd8d26c8bb8112ced7a5f48
momo_ <momo_@192.168.0.32>
2018-10-25 18:56:52 -0400

    This is a commit from another laptop

---
+++ src/PushingFromOtherPC.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PushingFromOtherPC {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Scanner console = new Scanner (System.in);
+		int num;
+		System.out.println("Please enter any number");
+		num = console.nextInt();
+
+	}
+}
